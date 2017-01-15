98506b5553ae2f8e978d1c557ad86d77891cef2d
Brian Barrow <briancbarrow@gmail.com>
2017-01-15 01:06:20 -0700

    working version

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'my-portfolio',
+    modulePrefix: 'ember-portfolio',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
